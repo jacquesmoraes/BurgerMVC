751667c96649ae3b6a3283b451cde9e6f62743ed
jacquesmoraes <jacquesbmoraes@hotmail.com>
2023-09-12 21:04:36 -0300

    components, invoke

---
+++ BurgerMVC/Components/CarrinhoCompraResumo.cs
@@ -0,0 +1,29 @@
+﻿using BurgerMVC.Models;
+using BurgerMVC.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BurgerMVC.Components
+{
+    public class CarrinhoCompraResumo : ViewComponent
+    {
+        private readonly CarrinhoCompra _carrinhoCompras;
+        public CarrinhoCompraResumo(CarrinhoCompra carrinhoCompra)
+        {
+            _carrinhoCompras = carrinhoCompra;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var itens = _carrinhoCompras.GetItensDoCarrinhos();
+            _carrinhoCompras.CarrinhoCompraItens = itens;
+
+            var itensVm = new CarrinhoCompraViewModel
+            {
+                carrinhoCompra = _carrinhoCompras,
+                CarrinhoCompraTotal = _carrinhoCompras.Total()
+            };
+
+            return View(itensVm);
+        }
+    }
+}
